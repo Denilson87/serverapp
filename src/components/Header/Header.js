4c960653ea9f9bdd1368e53ba8c37820a57b9a3e
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-08 12:41:25 -0700

    updating the header.css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import {Link, useLocation} from 'react-router-dom'
 import './Header.css';
 
 const Header = () => {
-    const [activeTab, setActivetab] = ("Home");
+    const [activeTab, setActivetab] = useState ("Home");
     return (
         <div className="header">
              <p className="logo">Server Management</p>
@@ -12,11 +12,11 @@ const Header = () => {
                      <p className={`${activeTab === "Home" ? "active" : ""}`} onClick={() => setActivetab("Home")}><b>Home</b></p>
                      <p className=""></p>
                  </Link>
-                 <Link to="/Add">
+                 <Link to="/add">
                      <p className={`${activeTab === "AddUser" ? "active" : ""}`} onClick={() => setActivetab("AddUser")}><b>Add-user</b></p>
                      <p className=""></p>
                  </Link>
-                 <Link to="/About">
+                 <Link to="/about">
                      <p className={`${activeTab === "About" ? "active" : ""}`} onClick={() => setActivetab("About")}><b>About</b></p>
                      <p className=""></p>
                  </Link>
