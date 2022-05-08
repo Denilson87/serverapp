abbd7ad2774529c68373c47c3cf08d2c6e337fe1
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-08 13:16:08 -0700

    fixing bug on link to routers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,19 @@ import {Link, useLocation} from 'react-router-dom'
 import './Header.css';
 
 const Header = () => {
-    const [activeTab, setActivetab] = useState ("Home");
+    const [activeTab, setActiveTab] = useState("Home");
     return (
         <div className="header">
              <p className="logo">Server Management</p>
              <div className="header-right">
                  <Link to="/">
-                     <p className={`${activeTab === "Home" ? "active" : ""}`} onClick={() => setActivetab("Home")}><b>Home</b></p>
-                     
+                     <p className={`${activeTab === "Home" ? "active" : ""}`} onClick={() => setActiveTab("Home")}><b>Home</b></p>                     
                  </Link>
                  <Link to="/add">
-                     <p className={`${activeTab === "AddUser" ? "active" : ""}`} onClick={() => setActivetab("AddUser")}><b>Add-user</b></p>
-                    
-                 </Link>
+                     <p className={`${activeTab === "AddUser" ? "active" : ""}`} onClick={() => setActiveTab("AddUser")}><b>AddUser</b></p>
+                    </Link>
                  <Link to="/about">
-                     <p className={`${activeTab === "About" ? "active" : ""}`} onClick={() => setActivetab("About")}><b>About</b></p>
-                    
+                     <p className={`${activeTab === "About" ? "active" : ""}`} onClick={() => setActiveTab("About")}><b>About</b></p>
                  </Link>
              </div>
         </div>
