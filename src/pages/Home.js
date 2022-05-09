49224d17871715a93865217e1b78dee95aae45fb
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-09 10:02:37 -0700

    updating the fetching

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,37 +19,35 @@ const Home = () => {
     };
     console.log("data=>", data);   
     return (
-        <div>Home Page</div>
-        // <div style={{marginTop:"150px"}}>
-        //     <table className="styled-table">
-        //         <thead>
-        //             <tr>
-        //                 <th style={{textAlign:"center"}}>ID</th>
-        //                 <th style={{textAlign:"center"}}>IP Addres</th>
-        //                 <th style={{textAlign:"center"}}>Name</th>
-        //                 <th style={{textAlign:"center"}}>Status</th>
-        //                 <th style={{textAlign:"center"}}>Type</th>
-        //                 <th style={{textAlign:"center"}}>Memory</th>                    
-        //             </tr>
-        //         </thead>
-        //         <tbody>
-        //             {data && data.map(data, index) =>{
-        //                 return (
-        //                     <tr key={index}>
-        //                         <th scope="row">{index + 1}</th>
-        //                         <td>{servers.id}</td>
-        //                         <td>{servers.ipAddress}</td>
-        //                         <td>{servers.name}</td>
-        //                         <td>{servers.memory}</td>
-        //                         <td>{servers.status}</td>
-        //                         <td>{servers.type}</td>
-        //                     </tr>
-        //                 )
-        //             })}
-        //         </tbody>
-        //     </table>
-        // </div>
-    )
-}
+        <div style={{marginTop:"150px"}}>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th style={{textAlign: "center"}}>
+                            ID                         
+                        </th>
+                        <th style={{textAlign: "center"}}>
+                            IP Address                        
+                        </th>
+                        <th style={{textAlign: "center"}}>
+                            Name                        
+                        </th>
+                        <th style={{textAlign: "center"}}>
+                            Status                         
+                        </th>
+                        <th style={{textAlign: "center"}}>
+                            Type                         
+                        </th>
+                        <th style={{textAlign: "center"}}>
+                            Action                         
+                        </th>
+                    </tr>
+                </thead>
+               
+            </table>
+        </div>
+        
+    );    
+};
 
-export default Home
+export default Home;
