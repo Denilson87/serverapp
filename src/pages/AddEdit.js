e7fbd584f4a97c8b417e07800ce4ff2e105ecf6d
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-12 03:28:54 -0700

    setting initail state forfom

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,7 +1,19 @@
 import React, { useState, useeFfect }from 'react';
 import { useHisotry, useLocation } from "react-router-dom";
+import axios from 'axois';
+import './AddEdit.css';
+
+
+const initialState = {
+    nome:"",
+    apelido:"",
+    telefone:"",
+    imageUrl:"",
+};
 
 const AddEdit = () => {
+const [state, setState] = useState(initialState);
+const [nome, apelido, telefone, imageUrl] = initialState;
     return (
         <div>
              <h2>This page is Edit and update !</h2>
