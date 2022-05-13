e01d79551298c3b4ae4777aadd9371c70db15db5
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-13 03:04:42 -0700

    updating app

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./AddEdit.css";
 import { toast } from 'react-toastify';
 
-const State = {
+const initialState = {
     nome:"",
     apelido:"",
     telefone:"",
@@ -13,8 +13,8 @@ const State = {
 
 const AddEdit = () => {
 
-const [state, setState] = useState(State);
-const {nome, apelido, telefone, imageUrl} = State;
+const [state, setState] = useState(initialState);
+const {nome, apelido, telefone, imageUrl} = initialState;
 //const history = useHistory();
 const addContact = async (data) => {
 const response = await axios.post("http://localhost:8080/", data);
@@ -39,14 +39,14 @@ const handleInputChange = (e) => {
         <div style={{marginTop: "100px"}}>
             <form  style={{margin:"auto", padding: "15px", maxWidth:"800px", alignContent:"center",}}  onSubmit={handleSubmit}>
             <label htmlFor="nome">Nome</label>
-            <input type="text" id="nome" name="nome" placeholder="Digite o nome..." onChange={handleInputChange} value={nome}/>
+            <input type="text" id="nome" name="nome" placeholder="Digite o nome..." />
             <label htmlFor="apelido">Apelido</label>
-            <input type="text" id="apelido" name="apelido" placeholder="Digite o apelido..." onChange={handleInputChange} value={apelido}/>
+            <input type="text" id="apelido" name="apelido" placeholder="Digite o apelido..." />
             <label htmlFor="telefone">Telefone</label>
-            <input type="text" id="telefone" name="telefone" placeholder="Digite o telefone..." onChange={handleInputChange} value={telefone}/>
+            <input type="text" id="telefone" name="telefone" placeholder="Digite o telefone..." />
             <label htmlFor="imageUrl">Link da Imagem</label>
-            <input type="text" id="imageUrl" name="imageUrl" placeholder="Coloque o link da imagem..." onChange={handleInputChange} value={imageUrl}/>
-            <input type="submit" value="Add"/>
+            <input type="text" id="imageUrl" name="imageUrl" placeholder="Coloque o link da imagem..." />
+            <input type="submit" value="Adicionar"/>
             </form>             
         </div>
     );
